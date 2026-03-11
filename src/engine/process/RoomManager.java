b822cbb445e4c960125b92da964731f2593a72f6
agnies <agnies.sadli@se.univ-bejaia.dz>
2026-03-11 10:03:45 +0100

    j'ai mis la classe RoomManager.java dans le package process

---
+++ src/engine/process/RoomManager.java
@@ -0,0 +1,56 @@
+package engine.process;
+
+import config.SimulationConfiguration;
+import engine.map.BathRoom;
+import engine.map.BedRoom;
+import engine.map.Kitchen;
+import engine.map.LivingRoom;
+
+public class RoomManager {
+    private final BedRoom bedroom;
+    private final BathRoom bathroom;
+    private final LivingRoom livingroom;
+    private final Kitchen kictchen;
+    private final int corridor;
+
+
+
+
+    public RoomManager(BedRoom bedroom, BathRoom bathroom, LivingRoom livingroom, Kitchen kictchen, int corridor) {
+        super();
+        this.bedroom = bedroom;
+        this.bathroom = bathroom;
+        this.livingroom = livingroom;
+        this.kictchen = kictchen;
+        this.corridor = SimulationConfiguration.LINE_COUNT/2;
+    }
+
+    public BedRoom getBedroom() {
+        return bedroom;
+    }
+
+
+    public BathRoom getBathroom() {
+        return bathroom;
+    }
+
+
+    public LivingRoom getLivingroom() {
+        return livingroom;
+    }
+
+
+    public Kitchen getKictchen() {
+        return kictchen;
+    }
+
+
+    public int getCorridor() {
+        return corridor;
+    }
+
+
+
+
+}
+
