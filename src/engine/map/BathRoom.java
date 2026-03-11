072332d64a0117051e75430806f7099e1742106a
mariam <mariamtraore24@outlook.com>
2026-03-11 10:14:17 +0100

    cc

--- src/engine/map/BathRoom.java
+++ src/engine/map/BathRoom.java
@@ -1,15 +1,13 @@
 package engine.map;
-
 public class BathRoom extends Room {
 
 	
 	
 	public BathRoom(int startLine, int startColumn, int endLine, int endColumn,
             Block door) {
-		 super(startLine,startColumn, endLine, endColumn, door);
+		 super(startLine, startColumn, endLine, endColumn, door);
 
     }
-		
 	}
 
 
